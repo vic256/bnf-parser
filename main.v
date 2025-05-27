1f2526d222628e524e5bdca484b1fa91d32972de
Victor G <victor@splrge.dev>
2025-05-27 14:33:30 +0000

    (WIP) refactor:  improve parsing

--- main.v
+++ main.v
@@ -4,7 +4,7 @@ type BnfValue = Ref | string | None
 type BnfToken = Bloc | BnfValue
 
 enum Op {
-	invalid_op
+	op_default
 	op_or
 	op_multiply
 	op_and
@@ -55,111 +55,71 @@ fn main() {
 
 	mut bnf_expressions := []BnfExpr{}
 	for key, value in expr {
+		new_value := (value + '\n')
+
 		mut bnf_expr := BnfExpr{} 
 		bnf_expr.key = key
 
 		mut current_bnf_bloc := Bloc{}
+		mut current_default_bnf_bloc := Bloc{[], Op.op_default}
 		mut current_bnf_value := BnfValue(None{})
 
 		mut entry := ''
-		for _, token in (value + '\n') {
-			if entry != '' && entry[entry.len - 1] == ` ` 
-				&& !(token in op_associations.keys()) // Token is not an operator
-				&& !(current_bnf_value is None) {
-				if current_bnf_bloc.operator == Op.invalid_op { current_bnf_bloc.operator = Op.op_and }
-				if current_bnf_bloc.operator == Op.op_and {
-					current_bnf_bloc.tokens << current_bnf_value
-					current_bnf_value = BnfValue(None{})
-					entry = ''
-				}
-			}
+		mut i := 0
+		for i < new_value.len {
+			token := new_value[i]
 
-			if token == `'` {
+			if token == ` ` {
+				for new_value[i] == ` ` {
+					i++
+				}
+				if !(new_value[i] in op_associations.keys()) {
+					if current_bnf_bloc.operator == Op.op_default { 
+						current_bnf_bloc.operator = Op.op_and 
+					}
+					if !(current_bnf_value is None) {
+						current_bnf_bloc.tokens << create_bnf_bloc(current_bnf_value)
+						current_bnf_value = BnfValue(None{})
+						entry = ''
+					}
+					if (current_bnf_value is None && entry != '') {
+						current_bnf_bloc.tokens << create_bnf_bloc(BnfValue(Ref{entry}))
+						entry = ''
+					}
+				}
+			} else if token == `'` {
 				if current_bnf_value.type_name() == 'None' {
 					current_bnf_value = BnfValue('')
 					entry = ''
-					continue
 				}
 				if current_bnf_value.type_name() == 'string' {
 					current_bnf_value = BnfValue(entry)
 					entry = ''
-					continue
 				}
+				i++
+			} else if token == `|` {
+				if current_bnf_bloc.operator == Op.op_default { current_bnf_bloc.operator = Op.op_or }
+				i++
+			} else if token == `*` {
+				if current_bnf_bloc.operator == Op.op_default { current_bnf_bloc.operator = Op.op_multiply }
+				i++
+			} else if token == `\n` {
+				if !(current_bnf_value is None) {
+					current_bnf_bloc.tokens << create_bnf_bloc(current_bnf_value)
+				}
+				if (current_bnf_value is None) && entry != '' {
+					current_bnf_bloc.tokens << create_bnf_bloc(BnfValue(Ref{entry}))
+				}
+				i++
+			} else {
+				entry += token.ascii_str()
+				i++
 			}
-			
-			if token == `|` && !(current_bnf_value is None) {
-				current_bnf_bloc.tokens << current_bnf_value
-				if current_bnf_bloc.operator == Op.invalid_op { current_bnf_bloc.operator = Op.op_or }
-				current_bnf_value = BnfValue(None{})
-				entry = ''
-				continue
-			}
-
-
-			if token.ascii_str() == '\n' {
-				current_bnf_bloc.tokens << current_bnf_value
-				continue
-			}
-
-			/*if token == ` ` && current_bnf_bloc.operator == Op.invalid_op && current_bnf_value.type_name() != 'None' {
-				current_bnf_bloc.tokens << current_bnf_value
-				if current_bnf_bloc.operator == Op.invalid_op { current_bnf_bloc.operator = Op.op_and }
-				current_bnf_value = BnfValue(None{})
-				entry = ''
-				continue
-			}*/
-
-
-			/*if op_associations[token] != Op.invalid_op && current_bnf_token.type_name() == 'None' {
-				bnf_expr.children << current_bnf_token
-				current_bnf_token = BnfToken(op_associations[token])
-				continue
-			}
-
-			if token == ` ` {
-				bnf_expr.children << current_bnf_token
-				current_bnf_token = BnfToken(None{})
-				entry = ''
-				continue
-			}*/
-			entry += token.ascii_str()
-
-			/*match token {
-				`'` {
-					if current_bnf_token.type_name() == 'None' {
-						current_bnf_token = BnfToken('')
-						continue
-					}
-					current_bnf_token = BnfToken(entry)
-				}
-				`|` {
-					if current_bnf_token.type_name() == 'string' { 
-						entry += token.ascii_str()
-						continue
-					 }
-					current_bnf_token = BnfToken(Op.op_or)
-				}
-				`*` {
-					if current_bnf_token.type_name() == 'string' {
-						entry += token.ascii_str()
-						continue
-					}
-					bnf_expr.children << BnfToken(Ref{entry})
-					current_bnf_token = BnfToken(Op.op_multiply)
-				}
-				` `{
-					bnf_expr.children << current_bnf_token
-					current_bnf_token = BnfToken(None{})
-					entry = ''
-				}
-				else {
-					entry += token.ascii_str()
-				}
-			}*/
 		}
 		println(current_bnf_bloc)
-		//bnf_expr.children << current_bnf_token
-		//bnf_expressions << bnf_expr
 	}
-			//println(bnf_expressions)
+}
+
+fn create_bnf_bloc(bnf_value BnfValue) Bloc {
+	return Bloc{[bnf_value], Op.op_default}
 }
